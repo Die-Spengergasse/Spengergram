3dcf436c5876fe623e2ca8b690cb97a3c91d8965
schrutek <martin@schrutek>
2024-05-26 20:12:56 +0200

    refactoring

--- Spg.Spengergram/src/Spg.Spengergram.Api/Program.cs
+++ Spg.Spengergram/src/Spg.Spengergram.Api/Program.cs
@@ -61,7 +61,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("api/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
--- Spg.Spengergram/src/Spg.Spengergram.Application/Parameter/FilterContainsOperations.cs
+++ Spg.Spengergram/src/Spg.Spengergram.Application/Parameter/FilterContainsOperations.cs
@@ -6,6 +6,7 @@ namespace Spg.Spengergram.Application.Parameter
     public class FilterContainsOperations : InterpretParameterBase<User>, IQueryParameter
     {
         private readonly IUserFilterBuilder _userFilterBuilder;
+        private object builder;
 
         public FilterContainsOperations(IUserFilterBuilder userFilterBuilder)
             : base("ct")
